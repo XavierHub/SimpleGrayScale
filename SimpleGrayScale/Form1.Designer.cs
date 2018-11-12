4abbe75ce1229619efa9e3bf094dfaae8462213a
Felixav <bestxavier16@yahoo.com.br>
2018-11-11 22:04:14 -0200

    Commit Inicial
    Commit Inicial

---
+++ SimpleGrayScale/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SimpleGrayScale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(413, 60);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Location = new System.Drawing.Point(35, 23);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(453, 20);
+            this.txtCaminho.TabIndex = 1;
+            this.txtCaminho.Text = "C:\\Users\\Xavier\\Desktop\\Fotos\\documento.tif";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 135);
+            this.Controls.Add(this.txtCaminho);
+            this.Controls.Add(this.btnConverter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.TextBox txtCaminho;
+    }
+}
+
---
+++ SimpleGrayScale/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleGrayScale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnConverter_Click(null, null);
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            string novoNome = Path.GetDirectoryName(txtCaminho.Text) + @"\gray_" + Path.GetFileName(txtCaminho.Text);
+            Bitmap c = new Bitmap(txtCaminho.Text);
+
+            //Image d = ToolStripRenderer.CreateDisabledImage(c);
+
+            //d.Save(Path.GetDirectoryName(txtCaminho.Text) + @"\grayRender_" + Path.GetFileName(txtCaminho.Text), System.Drawing.Imaging.ImageFormat.Tiff);
+
+            Bitmap grayScaled;
+            //Load an image frim you local file system
+            Image normalImage = Image.FromFile(txtCaminho.Text);
+            // Turn image into gray scale image
+            using (var img = new Bitmap(normalImage))
+            {
+                grayScaled = GrayscaleMatrix.Executar(img);
+            }
+
+            //CodecInfo para imagens Jpeg
+            ImageCodecInfo codec = ImageCodecInfo.GetImageEncoders().First(enc => enc.FormatID == ImageFormat.Jpeg.Guid);
+            //EncoderParameters que vai setar o nível de qualidade (compressão)
+            EncoderParameters imgParams = new EncoderParameters(1);
+            //Qualidade em 100L = 100% de qualidade - sem compressão
+            imgParams.Param = new[] { new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 55L) };
+            
+            //Salvar a imagem a imagem
+
+            if (File.Exists(novoNome)) File.Delete(novoNome);
+
+            grayScaled.Save(novoNome, codec, imgParams);
+        }
+    }
+}
